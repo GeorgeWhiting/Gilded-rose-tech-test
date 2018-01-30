9d872c2e688b22ea415e522acb0704324c14dae8
George <george_whiting@hotmail.com>
2018-01-30 11:25:55 +0000

    got the tests actually working

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -1,7 +1,4 @@
-require File.join(File.dirname(__FILE__), 'gilded_rose')
-
 describe GildedRose do
-
   describe "#update_quality" do
     it "does not change the name" do
       items = [Item.new("foo", 0, 0)]
