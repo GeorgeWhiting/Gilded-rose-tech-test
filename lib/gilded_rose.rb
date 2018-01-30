b637212fe34aa78fc51a563d52aaeeb555161547
George <george_whiting@hotmail.com>
2018-01-30 16:16:20 +0000

    removes magic strings

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,9 +1,12 @@
 class GildedRose
   SULFURAS = 'Sulfuras, Hand of Ragnaros'
+  BRIE = 'Aged Brie'
+  PASS = 'Backstage passes to a TAFKAL80ETC concert'
+
 
   def initialize(items)
     @items = items
-    @only_increase = ['Aged Brie', 'Backstage passes to a TAFKAL80ETC concert']
+    @only_increase = [BRIE, PASS]
   end
 
   def next_day
@@ -14,22 +17,22 @@ class GildedRose
 
   def increase_quality(item, amount)
     # amount *= 2 if item.sell_in < 0
-    item.quality += amount unless item.name == 'Sulfuras, Hand of Ragnaros'
+    item.quality += amount
     item.quality = 50 if item.quality > 50
   end
 
   def decrease_quality(item, amount)
     # amount *= 2 if item.sell_in < 0
-    item.quality -= amount unless item.name == 'Sulfuras, Hand of Ragnaros'
+    item.quality -= amount
     item.quality = 0 if item.quality < 0
   end
 
   def decrease_sellin(item, amount)
-    item.sell_in -= amount unless item.name == 'Sulfuras, Hand of Ragnaros'
+    item.sell_in -= amount
   end
 
   def backstage_pass_bonus_quality(item)
-    if item.name == 'Backstage passes to a TAFKAL80ETC concert'
+    if item.name == PASS
       increase_quality(item, 1) if item.sell_in < 11
       increase_quality(item, 1) if item.sell_in < 6
     end
@@ -40,6 +43,7 @@ class GildedRose
   end
 
   def update_quality(item)
+    return if item.name == SULFURAS
     if !@only_increase.include?(item.name)
       decrease_quality(item, 1)
     else
@@ -49,10 +53,16 @@ class GildedRose
     decrease_sellin(item, 1)
     if item_out_of_date?(item)
       decrease_quality(item, 1) unless @only_increase.include?(item.name)
-      increase_quality(item, 1) if item.name == 'Aged Brie'
-      decrease_quality(item, item.quality) if item.name == 'Backstage passes to a TAFKAL80ETC concert'
+      increase_quality(item, 1) if item.name == BRIE
+      decrease_quality(item, item.quality) if item.name == PASS
     end
   end
+
+  # def update_quality(item)
+  #   return if item.name == SULFURAS
+  #
+  #
+  # end
 end
 
 class Item
