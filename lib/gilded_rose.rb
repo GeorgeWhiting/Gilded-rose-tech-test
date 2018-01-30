8d0df98f95b57d2c7a157a3ad0f57f56bdad8f59
George <george_whiting@hotmail.com>
2018-01-30 16:09:48 +0000

    continues ripping apart the endless if stack

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,6 +1,9 @@
 class GildedRose
+  SULFURAS = 'Sulfuras, Hand of Ragnaros'
+
   def initialize(items)
     @items = items
+    @only_increase = ['Aged Brie', 'Backstage passes to a TAFKAL80ETC concert']
   end
 
   def next_day
@@ -25,27 +28,29 @@ class GildedRose
     item.sell_in -= amount unless item.name == 'Sulfuras, Hand of Ragnaros'
   end
 
+  def backstage_pass_bonus_quality(item)
+    if item.name == 'Backstage passes to a TAFKAL80ETC concert'
+      increase_quality(item, 1) if item.sell_in < 11
+      increase_quality(item, 1) if item.sell_in < 6
+    end
+  end
+
+  def item_out_of_date?(item)
+    item.sell_in < 0
+  end
+
   def update_quality(item)
-    if (item.name != 'Aged Brie') && (item.name != 'Backstage passes to a TAFKAL80ETC concert')
+    if !@only_increase.include?(item.name)
       decrease_quality(item, 1)
     else
       increase_quality(item, 1)
-      if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-        increase_quality(item, 1) if item.sell_in < 11
-        increase_quality(item, 1) if item.sell_in < 6
-      end
+      backstage_pass_bonus_quality(item)
     end
     decrease_sellin(item, 1)
-    if item.sell_in < 0
-      if item.name != 'Aged Brie'
-        if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-          decrease_quality(item, 1) if item.quality > 0
-        else
-          item.quality = item.quality - item.quality
-        end
-      else
-        increase_quality(item, 1)
-      end
+    if item_out_of_date?(item)
+      decrease_quality(item, 1) unless @only_increase.include?(item.name)
+      increase_quality(item, 1) if item.name == 'Aged Brie'
+      decrease_quality(item, item.quality) if item.name == 'Backstage passes to a TAFKAL80ETC concert'
     end
   end
 end
