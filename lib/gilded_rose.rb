8282d15c620a2ed04e12d0e6bdcc38d44e7e8a55
George <george_whiting@hotmail.com>
2018-01-30 17:11:09 +0000

    initial refactor complete, tests all passing

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -3,7 +3,6 @@ class GildedRose
   BRIE = 'Aged Brie'
   PASS = 'Backstage passes to a TAFKAL80ETC concert'
 
-
   def initialize(items)
     @items = items
     @only_increase = [BRIE, PASS]
@@ -16,13 +15,13 @@ class GildedRose
   end
 
   def increase_quality(item, amount)
-    # amount *= 2 if item.sell_in < 0
+    amount *= 2 if item_out_of_date?(item)
     item.quality += amount
     item.quality = 50 if item.quality > 50
   end
 
   def decrease_quality(item, amount)
-    # amount *= 2 if item.sell_in < 0
+    amount *= 2 if item_out_of_date?(item)
     item.quality -= amount
     item.quality = 0 if item.quality < 0
   end
@@ -32,37 +31,33 @@ class GildedRose
   end
 
   def backstage_pass_bonus_quality(item)
-    if item.name == PASS
-      increase_quality(item, 1) if item.sell_in < 11
-      increase_quality(item, 1) if item.sell_in < 6
-    end
+    increase_quality(item, 1) if item.sell_in < 11
+    increase_quality(item, 1) if item.sell_in < 6
+    item.quality = 0 if item_out_of_date?(item)
   end
 
   def item_out_of_date?(item)
     item.sell_in < 0
   end
 
-  def update_quality(item)
-    return if item.name == SULFURAS
-    if !@only_increase.include?(item.name)
-      decrease_quality(item, 1)
-    else
-      increase_quality(item, 1)
-      backstage_pass_bonus_quality(item)
-    end
-    decrease_sellin(item, 1)
-    if item_out_of_date?(item)
-      decrease_quality(item, 1) unless @only_increase.include?(item.name)
-      increase_quality(item, 1) if item.name == BRIE
-      decrease_quality(item, item.quality) if item.name == PASS
-    end
+  def quality_increases?(item)
+    @only_increase.include?(item.name)
   end
 
-  # def update_quality(item)
-  #   return if item.name == SULFURAS
-  #
-  #
-  # end
+  def maturing_items(item)
+    increase_quality(item, 1)
+    backstage_pass_bonus_quality(item) if item.name == PASS
+  end
+
+  def decaying_items(item)
+    item.name.include?("Conjured") ? decrease_quality(item, 2) : decrease_quality(item, 1)
+  end
+
+  def update_quality(item)
+    return if item.name == SULFURAS
+    decrease_sellin(item, 1)
+    quality_increases?(item) ? maturing_items(item) : decaying_items(item)
+  end
 end
 
 class Item
