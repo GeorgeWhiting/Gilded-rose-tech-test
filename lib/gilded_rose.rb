b124d77dd9e97e937300969d3094ccb64795c175
George <george_whiting@hotmail.com>
2018-01-31 11:38:49 +0000

    extracts item specific logic to multiple update_item methods

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -4,55 +4,55 @@ class GildedRose
     @items = items
   end
 
-  def update_quality()
+  def next_day
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
+      update_quality(item)
+    end
+  end
+
+  def update_normal_item(item)
+    item.sell_in -= 1
+    return if item.quality <= 0
+      item.quality -= 1
+      item.quality -= 1 if item.sell_in < 0
+  end
+
+  def update_brie(item)
+    item.sell_in -= 1
+    return if item.quality >= 50
+      item.quality += 1
+      item.quality += 1 if item.sell_in < 0
+  end
+
+  def update_pass(item)
+    item.sell_in -= 1
+    return item.quality = 0 if item.sell_in < 0
+    return if item.quality >= 50
+      item.quality += 1
+      item.quality += 1 if item.sell_in < 11
+      item.quality += 1 if item.sell_in < 6
+  end
+
+  def update_sulfuras(item)
+
+  end
+
+  def update_quality(item)
+
+    if item.name == "Glyph of Thorns"
+      return update_normal_item(item)
+    end
+    if item.name == "Aged Brie"
+      return update_brie(item)
+    end
+    if item.name == "Backstage passes to a TAFKAL80ETC concert"
+      return update_pass(item)
+    end
+    if item.name == "Sulfuras, Hand of Ragnaros"
+      return update_sulfuras(item)
     end
   end
 end
-
 class Item
   attr_accessor :name, :sell_in, :quality
 
